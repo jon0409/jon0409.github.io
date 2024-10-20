e5d12ceedc60b0a670316d60488ead10a67ff448
jon0409 <jonricpagapongomi@gmail.com>
2024-10-20 16:44:46 +0800

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const body = document.body
+
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanBye = document.querySelector("#bye")
+
+console.log(spanHi.dataset)
+spanHi.dataset.newName = 
